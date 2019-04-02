465ba70002270b30e246a4e5834d920f1f7cf663
jclane <justin@justinlane.me>
2019-04-02 14:41:09 +0000

    Trying to figure out how best to parse HTML

--- app/make_it_safe.py
+++ app/make_it_safe.py
@@ -1,13 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
+#import requests
+#from bs4 import BeautifulSoup
+from newspaper import Article
 
 
 def make_it_safe(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} # This is chrome, you can set whatever browser you like
-    req = requests.get(url, headers=headers)
-    soup = BeautifulSoup(req.content, features="html.parser")
-    site = soup.find("article")
-    title = site.find("h1", "entry-title")
-    content = site.find("div", "entry-content")
-
-    return {"title":title.get_text(), "content":content.get_text()}
+    #headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} # This is chrome, you can set whatever browser you like
+    #req = requests.get(url, headers=headers)
+    #soup = BeautifulSoup(req.content, features="html.parser")
+    #article = soup.find(id="article")
+    article = Article(url)
+    return {"article":article.html}
